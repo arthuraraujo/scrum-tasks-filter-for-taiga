2144af739ff28859c29c79b8dd29294d8f26972c
Arthur Araújo <contato@arthuraraujo.com.br>
2023-04-03 13:56:53 -0300

    .

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,7 +1,71 @@
-const currentUserName = document.querySelector(
-  '.dropdown-user > div > div > a'
-).innerText;
+// ////////////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////
+// Classes Observer, to later see if body has class loader-active
+// ////////////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////
+function observeClassChanges(element, callback) {
+  const observerCallback = (mutationsList) => {
+    for (const mutation of mutationsList) {
+      if (
+        mutation.type === 'attributes' &&
+        mutation.attributeName === 'class'
+      ) {
+        callback(element);
+      }
+    }
+  };
+  const config = { attributes: true, attributeFilter: ['class'] };
+  const observer = new MutationObserver(observerCallback);
+  observer.observe(element, config);
+  // Return a function to disconnect the observer when needed
+  return () => observer.disconnect();
+}
 
+// ////////////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////
+// Functions to verify if element exist and if the page is loading
+// ////////////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////
+
+function isTAIGA() {
+  const body = document.querySelector('body');
+  return body.hasAttribute('tg-main');
+}
+
+function isLoading() {
+  const body = document.querySelector('body');
+  return body.classList.contains('loader-active');
+}
+
+function isInTaskboard() {
+  const taskboardSection = document.querySelector('section.main.taskboard');
+  return !!taskboardSection;
+}
+
+function navExists() {
+  const navRight = document.querySelector('.nav-right');
+  return !!navRight;
+}
+
+// ////////////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////
+// DOM Manipulation Functions
+// ////////////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////
+
+// ////////////////////////////////////////////////////////////////////////////////////////////
+// Remove Filter to avoid duplicates
+// ////////////////////////////////////////////////////////////////////////////////////////////
+function removeFilterIfItExists() {
+  const filter = document.querySelector('.nav-right div > a[title="Filtros"]');
+  if (filter) {
+    filter.parentNode.removeChild(filter);
+  }
+}
+
+// ////////////////////////////////////////////////////////////////////////////////////////////
+// Hide elements from Taskboard (TaskboardRows and cards)
+// ////////////////////////////////////////////////////////////////////////////////////////////
 function hideTaskboardRows(userName) {
   const taskboardRows = document.querySelectorAll('.taskboard-row');
 
@@ -35,7 +99,44 @@ function hideTaskCards(userName) {
   });
 }
 
-function createTopNavDropdown(usernames) {
+// ////////////////////////////////////////////////////////////////////////////////////////////
+// Get TaskboardUsers and return an array of objects with name and src
+// ////////////////////////////////////////////////////////////////////////////////////////////
+function getUsersWithTasks() {
+  try {
+    const uniqueUserNames = new Set();
+    const cardUserAvatars = document.querySelectorAll('.card-user-avatar img');
+    cardUserAvatars.forEach((avatar) => {
+      const title = avatar.getAttribute('title');
+      const src = avatar.getAttribute('src');
+      const backgroundColor = avatar.style?.backgroundColor || 'rgb(0,0,0)';
+      uniqueUserNames.add(`${title} |  
+    ${src} | ${backgroundColor}`);
+    });
+
+    console.log(`uniqueUserNames: ${uniqueUserNames}`);
+    const sortedUniqueNames = Array.from(uniqueUserNames).sort();
+    const usersArrayOfObjects = sortedUniqueNames.map((user) => {
+      console.log(`user: ${user}`);
+      const usersArray = user.split('|');
+      return {
+        name: usersArray[0],
+        src: usersArray[1],
+        backgroundColor: usersArray[2],
+      };
+    });
+    return usersArrayOfObjects;
+  } catch (e) {
+    console.log('e1', e);
+  }
+}
+
+// ////////////////////////////////////////////////////////////////////////////////////////////
+// Create the filter and insert it in the TopNav
+// ////////////////////////////////////////////////////////////////////////////////////////////
+
+function createTopNavDropdown(usersArrayOfObjects) {
+  removeFilterIfItExists();
   const navRight = document.querySelector('.nav-right');
 
   // Cria a estrutura HTML com os elementos necessários
@@ -46,21 +147,38 @@ function createTopNavDropdown(usernames) {
   a.href = '#';
   a.target = '_blank';
   a.title = 'Filtros';
+  a.onclick = function (e) {
+    e.preventDefault();
+    return false;
+  };
   topnavDropdownWrapper.appendChild(a);
 
-  const tgSvg = document.createElement('tg-svg');
-  tgSvg.setAttribute('svg-icon', 'icon-filters');
-  a.appendChild(tgSvg);
+  const filterIconSVG = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m17 21l1.8 1.77c.5.5 1.2.1 1.2-.49V18l2.8-3.4A1 1 0 0 0 22 13h-7c-.8 0-1.3 1-.8 1.6L17 18v3m-2-1H2v-3c0-2.7 5.3-4 8-4c.6 0 1.3.1 2.1.2c-.2.6-.1 1.3.1 1.9c-.7-.1-1.5-.2-2.2-.2c-3 0-6.1 1.5-6.1 2.1v1.1h10.6l.5.6V20M10 4C7.8 4 6 5.8 6 8s1.8 4 4 4s4-1.8 4-4s-1.8-4-4-4m0 6c-1.1 0-2-.9-2-2s.9-2 2-2s2 .9 2 2s-.9 2-2 2Z"/></svg>
+  `;
 
-  const svg = document.createElement('svg');
-  svg.className = 'icon icon-filters';
-  svg.style.fill = ` `;
-  tgSvg.appendChild(svg);
+  a.insertAdjacentHTML('beforeend', filterIconSVG);
 
-  const use = document.createElement('use');
-  use.setAttribute('xlink:href', '#icon-filters');
-  use.setAttribute('href', '#icon-filters');
-  svg.appendChild(use);
+  const addAllUsersOptionToList = (ListElement) => {
+    const li = document.createElement('li');
+    ListElement.appendChild(li);
+
+    const userLink = document.createElement('a');
+    userLink.href = '#';
+    userLink.className = 'secondary';
+    userLink.onclick = function (e) {
+      e.preventDefault();
+      hideTaskboardRows();
+      hideTaskCards();
+      return false;
+    };
+    li.appendChild(userLink);
+
+    const usernameSpan = document.createElement('span');
+    usernameSpan.textContent = '--- Ver todos ---';
+    userLink.style.justifyContent = 'center';
+    userLink.appendChild(usernameSpan);
+  };
 
   const navbarDropdown = document.createElement('div');
   navbarDropdown.className = 'navbar-dropdown navigation-help';
@@ -70,7 +188,7 @@ function createTopNavDropdown(usernames) {
   navbarDropdown.appendChild(ul);
 
   // Cria os itens da lista com base no array de nomes de usuários
-  usernames.forEach((username) => {
+  usersArrayOfObjects.forEach((userObject) => {
     const li = document.createElement('li');
     ul.appendChild(li);
 
@@ -79,30 +197,35 @@ function createTopNavDropdown(usernames) {
     userLink.className = 'secondary';
     userLink.onclick = function (e) {
       e.preventDefault();
-      hideTaskboardRows(username);
-      hideTaskCards(username);
+      hideTaskboardRows(userObject.name);
+      hideTaskCards(userObject.name);
       return false;
     };
     li.appendChild(userLink);
 
-    const userTgSvg = document.createElement('tg-svg');
-    userTgSvg.setAttribute('svg-icon', 'icon-user');
-    userLink.appendChild(userTgSvg);
+    const avatar = document.createElement('img');
+    try {
+      if (userObject.backgroundColor) {
+        avatar.style.backgroundColor = userObject.backgroundColor;
+      }
+    } catch (e) {
+      console.log('e2', e);
+    }
+    avatar.src = userObject.src;
+    avatar.style.width = '40px';
+    avatar.style.height = '40px';
+    avatar.style.borderRadius = '50%';
+    avatar.style.marginRight = '10px';
+    avatar.style.objectFit = 'cover';
 
-    const userSvg = document.createElement('svg');
-    userSvg.className = 'icon icon-user';
-    userSvg.style.fill = ` `;
-    userTgSvg.appendChild(userSvg);
-
-    const userUse = document.createElement('use');
-    userUse.setAttribute('xlink:href', '#icon-user');
-    userUse.setAttribute('href', '#icon-user');
-    userSvg.appendChild(userUse);
+    userLink.appendChild(avatar);
 
     const usernameSpan = document.createElement('span');
-    usernameSpan.textContent = username;
+    usernameSpan.textContent = userObject.name;
     userLink.appendChild(usernameSpan);
   });
+
+  addAllUsersOptionToList(ul);
 
   const comment = document.createComment('');
   //   navRight.appendChild(comment);
@@ -112,26 +235,30 @@ function createTopNavDropdown(usernames) {
   navRight.insertBefore(topnavDropdownWrapper, comment);
 }
 
-function getUsersWithTasks() {
-  const uniqueUserNames = new Set();
-  const cardUserAvatars = document.querySelectorAll('.card-user-avatar img');
-  cardUserAvatars.forEach((avatar) => {
-    const title = avatar.getAttribute('title');
-    uniqueUserNames.add(title);
-  });
-  const sortedUniqueNames = Array.from(uniqueUserNames).sort();
-  return sortedUniqueNames;
+// ////////////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////
+// Add Filter only on the correct Taiga Page and only if the nav exists and is not loading
+// ////////////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////
+function addFilterIfInTaskboard() {
+  if (isTAIGA() && !isLoading() && navExists()) {
+    if (isInTaskboard()) {
+      const usersWithTasks = getUsersWithTasks();
+      createTopNavDropdown(usersWithTasks);
+    } else {
+      removeFilterIfItExists();
+    }
+  }
 }
 
-const usersWithTasks = getUsersWithTasks();
-createTopNavDropdown(usersWithTasks);
-
-// console.log('ok');
-// document.addEventListener('DOMContentLoaded', () => {
-//   // Executa a função para ocultar as divs "taskboard-inner" sem a imagem "Arthur Araújo" quando a página estiver montada
-//   // hideTaskboardRows();
-//   // createDropdown()
-//   console.log('DOM loaded');
-//   const usersWithTasks = getUsersWithTasks();
-//   createTopNavDropdown(usersWithTasks);
-// });
+// ////////////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////
+// Run only if its a Taiga Webpaige
+// ////////////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////
+if (isTAIGA()) {
+  // const observer = new MutationObserver(addFilterIfInTaskboard);
+  // observer.observe(document.body, { subtree: true, childList: true });
+  const body = document.querySelector('body');
+  observeClassChanges(body, addFilterIfInTaskboard);
+}
